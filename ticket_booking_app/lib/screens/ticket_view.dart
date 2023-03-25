f40c4d5a3432c64899d51283048b08c3a6ddcd84
jalala-pv <pvjalala@gmail.com>
2023-03-25 07:34:59 +0530

    aaa

--- ticket_booking_app/lib/screens/ticket_view.dart
+++ ticket_booking_app/lib/screens/ticket_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_booking_app/utils/app_layout.dart';
 import 'package:ticket_booking_app/utils/app_styles.dart';
+import 'package:ticket_booking_app/utils/widgets/thick_container.dart';
 
 class TicketView extends StatelessWidget {
   @override
@@ -9,20 +10,42 @@ class TicketView extends StatelessWidget {
     return Container(
       width: size.width,
       height: 200,
+      padding: EdgeInsets.all(16),
       child: Column(
         children: [
           Container(
+            decoration: BoxDecoration(
+                color: Color(0xFF526799),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10))),
             padding: EdgeInsets.all(20),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
                   'NYC',
-                  style: Styles.headlineStyle3,
+                  style: Styles.headlineStyle3.copyWith(color: Colors.white),
                 ),
+                Expanded(child: Container()),
+                ThickContainer(),
+                Expanded(
+                    child: SizedBox(
+                  height: 24,
+                  child: LayoutBuilder(
+                    builder: (BuildContext context, BoxConstraints constraints) {
+                      print('the width ${constraints.constrainWidth()}');
+                      return Flex(
+                          direction: Axis.horizontal,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: List.generate(5, (index) => Text('-')));
+                    },
+                  ),
+                )),
+                ThickContainer(),
+                Expanded(child: Container()),
                 Text(
                   'LDN',
-                  style: Styles.headlineStyle3,
+                  style: Styles.headlineStyle3.copyWith(color: Colors.white),
                 ),
               ],
             ),
