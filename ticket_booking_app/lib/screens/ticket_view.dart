6f1a95265f0e3009a4b9ae7710ee588b3a011bbc
jalala-pv <pvjalala@gmail.com>
2023-03-27 08:14:18 +0530

    aaa

--- ticket_booking_app/lib/screens/ticket_view.dart
+++ ticket_booking_app/lib/screens/ticket_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:ticket_booking_app/utils/app_layout.dart';
 import 'package:ticket_booking_app/utils/app_styles.dart';
 import 'package:ticket_booking_app/utils/widgets/thick_container.dart';
@@ -13,6 +14,7 @@ class TicketView extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: Column(
         children: [
+          /*shows the blue part of the card*/
           Container(
             decoration: BoxDecoration(
                 color: Color(0xFF526799),
@@ -20,38 +22,126 @@ class TicketView extends StatelessWidget {
                     topLeft: Radius.circular(10),
                     topRight: Radius.circular(10))),
             padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'NYC',
+                      style:
+                          Styles.headlineStyle3.copyWith(color: Colors.white),
+                    ),
+                    Expanded(child: Container()),
+                    ThickContainer(),
+                    Expanded(
+                        child: Stack(children: [
+                      SizedBox(height: 24, child: linebuild(6)),
+                      Center(
+                        child: Transform.rotate(
+                          angle: 1.5,
+                          child: Icon(
+                            Icons.local_airport_rounded,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ])),
+                    ThickContainer(),
+                    Expanded(child: Container()),
+                    Text(
+                      'LDN',
+                      style:
+                          Styles.headlineStyle3.copyWith(color: Colors.white),
+                    ),
+                  ],
+                ),
+                Gap(3),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'New-York',
+                      style:
+                          Styles.headlineStyle4.copyWith(color: Colors.white),
+                    ),
+                    Text(
+                      '8H 30M',
+                      style:
+                          Styles.headlineStyle4.copyWith(color: Colors.white),
+                    ),
+                    Text(
+                      'London',
+                      style:
+                          Styles.headlineStyle4.copyWith(color: Colors.white),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          /* 
+        shows the orange part of the card
+       */
+          Container(
+            // height: 80,
+            // width: size.width,
+            decoration: BoxDecoration(color: Styles.orangeColor),
             child: Row(
               children: [
-                Text(
-                  'NYC',
-                  style: Styles.headlineStyle3.copyWith(color: Colors.white),
+                SizedBox(
+                  height: 20,
+                  width: 10,
+                  child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10),
+                              bottomRight: Radius.circular(10)))),
                 ),
-                Expanded(child: Container()),
-                ThickContainer(),
                 Expanded(
-                    child: SizedBox(
-                  height: 24,
-                  child: LayoutBuilder(
-                    builder: (BuildContext context, BoxConstraints constraints) {
-                      print('the width ${constraints.constrainWidth()}');
-                      return Flex(
-                          direction: Axis.horizontal,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: List.generate(5, (index) => Text('-')));
-                    },
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: linebuild(15),
                   ),
-                )),
-                ThickContainer(),
-                Expanded(child: Container()),
-                Text(
-                  'LDN',
-                  style: Styles.headlineStyle3.copyWith(color: Colors.white),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 10,
+                  child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)))),
                 ),
               ],
             ),
-          )
+          ),
+          /*shows the botton orange card*/
+          Container(),
         ],
       ),
+    );
+  }
+
+  Widget linebuild(double x) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Flex(
+          direction: Axis.horizontal,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: List.generate(
+            (constraints.constrainWidth() / x).floor(),
+            (index) => SizedBox(
+              width: 3,
+              height: 1,
+              child:
+                  DecoratedBox(decoration: BoxDecoration(color: Colors.white)),
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- ticket_booking_app/lib/utils/app_styles.dart
+++ ticket_booking_app/lib/utils/app_styles.dart
@@ -4,10 +4,10 @@ class Styles{
   static Color primaryColor=primary;
   static Color TextColor=const Color(0xFF3b3b3b);
   static Color bgcolor=const Color(0xFFeeedf2);
-  static Color orangeColor=const  Color(0xFF526799);
+  static Color orangeColor=Color.fromARGB(255, 239, 73, 51);
 static TextStyle textStyle=TextStyle(fontSize: 16,color:TextColor,fontWeight: FontWeight.w500);
 static TextStyle headlineStyle1=TextStyle(fontSize: 26,color:TextColor,fontWeight: FontWeight.bold);
 static TextStyle headlineStyle2=TextStyle(fontSize: 21,color:TextColor,fontWeight: FontWeight.w500);
 static TextStyle headlineStyle3=TextStyle(fontSize: 17,color:Colors.grey.shade500,fontWeight: FontWeight.w500);
-static TextStyle headlineStyle4=TextStyle(fontSize: 17,color:Colors.grey.shade500,fontWeight: FontWeight.w500);
+static TextStyle headlineStyle4=TextStyle(fontSize: 15,color:Colors.grey.shade500,fontWeight: FontWeight.w500);
 }
